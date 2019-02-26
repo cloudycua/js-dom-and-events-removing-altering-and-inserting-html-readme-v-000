97692faeabb2b2872e43ce363ca76c42ea0f582a
Christine Cua <cloudycua@gmail.com>
2019-02-26 15:51:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 
-let element = document.createElement('div');
+$(document).ready(function() {
+  let element = document.createElement('div');
 
-element.innerHTML = 'Hello, DOM!';
+  element.innerHTML = 'Hello, DOM!';
+})
