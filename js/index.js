bae70265f77df530a3df3d144a3745f85bad2223
Christine Cua <cloudycua@gmail.com>
2019-02-26 15:56:17 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -6,4 +6,7 @@ let element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = '#f9f9f9';
 
+// Append element to body so it appears in the DOM
 document.body.appendChild(element);
+
+element.style.textAlign = 'center';
