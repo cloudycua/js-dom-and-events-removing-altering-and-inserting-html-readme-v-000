0dc937f6ac6ec1192c70c6617df0ff4f1e921448
Christine Cua <cloudycua@gmail.com>
2019-02-26 16:02:38 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
+
+let element = document.createElement('div');
+
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
+// Append element to body so it appears in the DOM
+document.body.appendChild(element);
+
+element.style.textAlign = 'center';
+
+let ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+ul.style.textAlign = 'left';
+
+// remove second element
+ul.removeChild(ul.querySelector('li:nth-child(2)'));
+
+// remove whole unordered list
+ul.remove();
