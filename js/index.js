a47b17a279785b56d2518b5be5470d42704cd85e
Christine Cua <cloudycua@gmail.com>
2019-02-26 15:55:23 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 //this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
 
-// $(document).ready(function() {
-  let element = document.createElement('div');
+let element = document.createElement('div');
 
-  element.innerHTML = 'Hello, DOM!';
-  element.style.backgroundColor = '#f9f9f9';
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
 
-  document.body.appendChild(element);
-// })
+document.body.appendChild(element);
+
